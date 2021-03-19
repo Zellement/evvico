a73e03c3c376811141bc8acb6f7b87c6043e0e68
Dan Farrow <dan.farrow@adtrak.co.uk>
2021-03-19 18:13:15 +0000

    layouts

--- components/header.js
+++ components/header.js
@@ -4,14 +4,16 @@ import cn from "classnames"
 import Container from "./container"
 import Email from "./atoms/email"
 import Image from "next/image"
+import WhatsApp from "./atoms/whatsapp"
 import ReactCountryFlag from "react-country-flag"
 import { HiOutlineMenuAlt2 } from "react-icons/hi"
+import { IoMdClose } from "react-icons/io"
 
 export default function Header() {
   const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false)
   return (
     <>
-      <div className="fixed top-0 left-0 items-center justify-end hidden w-full p-4 text-white bg-black lg:flex">
+      <div className="fixed top-0 left-0 z-40 items-center justify-end hidden w-full p-4 text-white bg-black lg:flex">
         <div className="">
           <Email />
         </div>
@@ -44,14 +46,14 @@ export default function Header() {
             svg
           />
         </div>
-        <div className="">Whatsapp</div>
+        <div className=""><WhatsApp /></div>
         <div className="">0115 000 0000</div>
       </div>
-      <header className="py-4 mb-4 md:mb-6 xl:mb-8 lg:mt-16">
+      <header className="py-4 mb-4 md:mb-6 xl:mb-8 lg:mt-28">
         <Container>
           <div className="flex flex-wrap">
             <Link href="/">
-              <a aria-label="Go to the homepage" className="w-40">
+              <a aria-label="Go to the homepage" className="max-w-full mx-auto w-72 lg:m-0">
                 <Image
                   src="/evvico-logo.png"
                   width={700}
@@ -64,28 +66,64 @@ export default function Header() {
             </Link>
             <div
               className={cn(
-                "absolute top-0 bg-black text-white w-full h-screen overflow-y-auto z-30 p-4 text-lg transform transition-300",
+                "absolute top-0 bg-black text-white w-full h-screen overflow-y-auto z-30 p-8 text-lg transform transition-300",
                 mobileMenuIsOpen ? `left-0` : `-left-full`
               )}
             >
               <button
-                className="absolute top-0 right-0 z-50 mt-4 mr-4"
+                className="absolute top-0 right-0 z-50 flex items-center mt-4 mr-4 space-x-2 bg-blue-100 text-black p-4"
                 onClick={() => setMobileMenuIsOpen(!mobileMenuIsOpen)}
               >
-                Close
+                <IoMdClose className="" /> <span>Close</span>
               </button>
               <nav>
-                <ul className="flex flex-col w-10/12">
-                  {[
-                    { title: "Home", route: "/" },
-                    { title: "About", route: "/about" },
-                  ].map(({ route, title }) => (
-                    <li className="mt-3 md:mt-0 md:ml-6" key={title}>
-                      <Link href={route}>
-                        <a className="block text-white">{title}</a>
+                <ul className="flex flex-col w-9/12">
+                    <li className="">
+                      <Link href="/">
+                        <a className="block py-3 leading-tight border-b border-gray-800">Home</a>
                       </Link>
                     </li>
-                  ))}
+                    <li className="">
+                      <Link href="/home-ev-chargers">
+                        <a className="block py-3 leading-tight border-b border-gray-800"><span className="block text-sm text-blue-100">Charging Points</span> for the Home</a>
+                      </Link>
+                    </li>
+                    <li className="">
+                      <Link href="/work-ev-chargers">
+                        <a className="block py-3 leading-tight border-b border-gray-800"><span className="block text-sm text-blue-100">Charging Points</span> for the Workplace</a>
+                      </Link>
+                    </li>
+                    <li className="">
+                      <Link href="/public-ev-chargers">
+                        <a className="block py-3 leading-tight border-b border-gray-800"><span className="block text-sm text-blue-100">Charging Points</span> for Public Spaces</a>
+                      </Link>
+                    </li>
+                    <li className="">
+                      <Link href="/faqs">
+                        <a className="block py-3 leading-tight border-b border-gray-800">FAQs</a>
+                      </Link>
+                    </li>
+                    <li className="">
+                      <Link href="/why-choose-us">
+                        <a className="block py-3 leading-tight border-b border-gray-800">Why Choose Us?</a>
+                      </Link>
+                    </li>
+                    <li className="">
+                      <Link href="/about-us">
+                        <a className="block py-3 leading-tight border-b border-gray-800">About Us</a>
+                      </Link>
+                    </li>
+                    <li className="">
+                      <Link href="/make-an-enquiry">
+                        <a className="block py-3 leading-tight border-b border-gray-800">Make an Enquiry</a>
+                      </Link>
+                    </li>
+                    <li className="">
+                      <Link href="/">
+                        <a className="block py-3 leading-tight border-b border-gray-800">Home</a>
+                      </Link>
+                    </li>
+                    
                 </ul>
               </nav>
             </div>
@@ -122,7 +160,7 @@ export default function Header() {
             svg
           />
         </div>
-        <div className="">Whatsapp</div>
+        <div className=""><WhatsApp /></div>
         <div className="">0115 000 0000</div>
         <button
           className="flex items-center p-2 space-x-2 text-black bg-blue-100"

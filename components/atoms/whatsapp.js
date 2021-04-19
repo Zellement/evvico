df9570bc8c1e60360f94e71a5d965d5e35aca385
Dan Farrow <dan.farrow@adtrak.co.uk>
2021-04-19 15:51:46 +0100

    change whatsapp to international style

--- components/atoms/whatsapp.js
+++ components/atoms/whatsapp.js
@@ -6,7 +6,7 @@ export default function WhatsApp() {
     <>
       <a
         className="flex flex-row items-center space-x-2 hover:text-green-300"
-        href="https://wa.me/07598069741"
+        href="https://wa.me/447598069741"
       >
         <AiOutlineWhatsApp className="-mt-1 text-green-300" /> <span>WhatsApp</span>
       </a>

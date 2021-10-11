714d01813b03bb923a7b6e19c313540938352847
Dan Farrow <dan.farrow@adtrak.co.uk>
2021-10-11 15:31:26 +0100

    minor ui

--- components/atoms/email.js
+++ components/atoms/email.js
@@ -1,5 +1,5 @@
 export default function Email({className}){
   return (
-    <a className={className} href="mailto:enquiries@evvico.ltd">enquiries@evvico.ltd</a>
+    <a className={className} href="mailto:enquiries@effico.ltd">enquiries@effico.ltd</a>
   )
 }
--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: 'https://www.evvico.ltd',
+  siteUrl: 'https://www.evvico.co.uk',
   generateRobotsTxt: true, // (optional)
 }
